03df6153a5d4daa38e74c28be1f2ac7bbcc29d48
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-02 07:20:58 -0500

    Upload from Lambda

---
+++ js/index.js
@@ -0,0 +1,9 @@
+{
+    "heroImage": "images/index1.jpg",
+    "showcaseImage": "images/index2.jpg",
+    "icons": {
+        "cloud": "images/icon-cloud.svg",
+        "shield": "images/icon-shield.svg",
+        "scale": "images/icon-scale.svg"
+    }
+}
