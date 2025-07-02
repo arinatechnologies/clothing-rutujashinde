29dfadb1df501528a7556a42ce4b9495b721ee1e
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-02 07:20:35 -0500

    Upload from Lambda

---
+++ js/gallery.js
@@ -0,0 +1,17 @@
+[
+  "luxury silk evening gown on runway model",
+  "tailored navy blue wool suit with pocket square",
+  "handcrafted leather tote bag with gold hardware",
+  "organic cotton loungewear set in neutral tones",
+  "limited edition sequin cocktail dress backstage",
+  "sustainable bamboo fiber shirt detail shot",
+  "oversized cashmere coat on city streets",
+  "artisanal hand-knit sweater close-up",
+  "minimalist leather belt with brass buckle",
+  "couture bridal gown with intricate beading",
+  "recycled polyester activewear set in motion",
+  "vintage-inspired tweed blazer with elbow patches",
+  "handwoven silk scarf draped artistically",
+  "architectural pleated midi skirt detail",
+  "signature Rutuja logo-embroidered denim jacket"
+]
